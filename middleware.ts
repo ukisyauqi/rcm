e3785cfdb3b8c7c8d9f5a7ed41dc548f9d91bd46
M. Syauqi Fadhlika <ukisyauqi@gmail.com>
2024-08-01 08:54:46 +0700

    fix middleware, fix image not loaded

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,12 @@
-import { NextResponse } from "next/server"
-import { auth } from "./auth"
+import { auth } from "./auth";
 
 export default auth((req) => {
-  const { pathname } = req.nextUrl
-  if (
-    pathname.startsWith("api") ||
-    pathname.startsWith("_next/static") ||
-    pathname.startsWith("_next/image") ||
-    pathname.startsWith("favicon.ico")
-  ) {
-    return NextResponse.next()
-  }
   if (!req.auth && req.nextUrl.pathname !== "/login") {
     const newUrl = new URL("/login", req.nextUrl.origin)
     return Response.redirect(newUrl)
   }
 })
 
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|public).*)"],
-// }
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|public).*)"],
+}
